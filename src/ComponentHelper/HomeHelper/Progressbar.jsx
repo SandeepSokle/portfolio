58e93efef00105a9d7b002164fc4d1fad50b0e3b
Sandeep Sokle <sandeepsokle12@gmail.com>
2021-10-29 22:42:16 +0530

    up

--- src/ComponentHelper/HomeHelper/Progressbar.jsx
+++ src/ComponentHelper/HomeHelper/Progressbar.jsx
@@ -1,25 +1,48 @@
 import "../../SCSS/HomeHelper/Progressbar.scss";
+import { useState, useEffect } from "react";
 
+export const Progressbar = () => {
+  let [progress, setProgress] = useState(0);
 
+useEffect(() => {
+    console.log("rendered");
+    if(progress === 10){
+       let ele = document.querySelector(".frontCurve");
+       ele.style.display = "block";
+       ele.style.width = "2rem";
+    }
+    else if(progress>10) {
+        let ele = document.querySelector(".frontCurve");
+        ele.style.display = "block";
+        ele.style.width = "2rem";
+        let p = document.querySelector(".progressbar");
+        p.style.display="block";
+        p.style.width = `${progress}%`;
+    }
+    
+}, [progress]);
+useEffect(()=>{
+    console.log(progress);
+   let id = setInterval(()=> {
+        let p = progress;
+        setProgress((p+1)%100);
+    },1000)
+    return ()=>{
+        clearInterval(id);
+    }
+});
+  // setInterval(() => {
+  //   setProgress((progress + 1) % 100);
+  // }, 200);
+  const progressStyle = {
+    width: `${progress}%`,
+  };
 
-export const Progressbar = ()=>{
-
-
-    const progressStyle = {
-        color: "blue",
-        width: "70%",
-        Height : "100%",
-        
-    };
-    const progressContainerStyle = {
-        color: "white",
-        outerHeight : "4rem",
-        width: "100%",
-        border  : "2px solid black"
-      };
-
-
-    return <div className="progressContainer" style = {progressContainerStyle}>
-        <div className="progressbar" style = {progressStyle}></div>
+  return (
+    <div className="progressContainer">
+      <div className="frontCurve"></div>
+      <div className="progressbar" ></div>
+      <div className="endCurve"></div>
     </div>
-}
+  );
+};
